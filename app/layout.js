98af870aac41449bc99189d69d27a17d81f3b1a0
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2025-01-21 14:59:20 +0530

    nextjs repo setup done

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./styles/globals.css";
+import StoreProvide from "./StoreProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "resorce allocations",
+  description: "Resorce Allocations",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <StoreProvide>
+        {children}
+        </StoreProvide>
+      </body>
+    </html>
+  );
+}
