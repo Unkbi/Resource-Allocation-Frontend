dde07d6c24373b90741daf6b100e5714dbca980d
Sahadev Tandur <sahadev.tandur@autodesk.com>
2025-04-02 18:26:58 +0530

    Basic File Structue.

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 
 interface InputProps {
-    label: string;
-    variant?: 'outlined' | 'filled' | 'standard';
-    [key: string]: any; // Allow additional props
+  label: string;
+  variant?: 'outlined' | 'filled' | 'standard';
+  [key: string]: any; // Allow additional props
 }
 
-const CustomInput: React.FC<InputProps> = ({ label, variant = 'outlined', ...props }) => {
-    return <TextField label={label} variant={variant} {...props} />;
+const CustomInput: React.FC<InputProps> = ({
+  label,
+  variant = 'outlined',
+  ...props
+}) => {
+  return <TextField label={label} variant={variant} {...props} />;
 };
 
 export default CustomInput;
---
+++ app/types/api.ts
@@ -0,0 +1,1 @@
+// Here the API Types are defined
