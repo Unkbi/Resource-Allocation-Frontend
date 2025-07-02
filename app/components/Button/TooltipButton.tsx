c8dde818f8905469df1c30d29526837f90e3c6cd
Sahadev Tandur <sahadev.tandur@autodesk.com>
2025-07-02 11:49:12 +0530

    Removed unecessary imports to tooltipButton.

--- app/components/Button/TooltipButton.tsx
+++ app/components/Button/TooltipButton.tsx
@@ -1,6 +1,12 @@
 import React, { ReactNode, MouseEventHandler } from 'react';
-import { Tooltip, Button, styled, tooltipClasses, TooltipProps,
-  ButtonProps, } from '@mui/material';
+import {
+  Tooltip,
+  Button,
+  styled,
+  tooltipClasses,
+  TooltipProps,
+  ButtonProps,
+} from '@mui/material';
 
 const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
@@ -20,7 +26,13 @@ interface TooltipButtonProps extends ButtonProps {
   children: ReactNode;
 }
 
-const TooltipButton: React.FC<TooltipButtonProps> = ({ msg, className, onClick, children, ...rest }) => {
+const TooltipButton: React.FC<TooltipButtonProps> = ({
+  msg,
+  className,
+  onClick,
+  children,
+  ...rest
+}) => {
   return (
     <StyledTooltip title={msg} placement="top" arrow>
       <Button className={className} {...rest} onClick={onClick}>
