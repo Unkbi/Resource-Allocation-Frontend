509aacdea0569b673ea748b247b9a3d8192e6a5b
Sahadev Tandur <sahadev.tandur@autodesk.com>
2025-07-21 09:37:37 +0530

    Test Deployment

--- app/(root)/allocation/page.tsx
+++ app/(root)/allocation/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import AllocationInit from './init';
 
+// To Be Deleted.
 export default function AllocationPage() {
   return (
     <Suspense fallback={<div>Loading allocation...</div>}>
