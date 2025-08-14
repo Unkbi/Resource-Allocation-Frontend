48cc4742bd4a2aa2290021f6f70802b4e19c61a4
Sahadev Tandur <sahadev.tandur@autodesk.com>
2025-08-14 03:43:03 -0400

    Code Clean up

--- app/redux/sagas/organisationsSaga.ts
+++ app/redux/sagas/organisationsSaga.ts
@@ -4,10 +4,8 @@ import {
  DELETE_ORGANISATION,
  FETCH_ORGANISATIONS,
  UPDATE_ORGANISATION,
-//  FETCH_ORGANISATIONS_RESOURCES,
 } from '../actions/organizationsAction';
 import {
- setAllOrganisationsResources,
  setLoading,
  setOrganisations,
 } from '../reducers/oraganisationsReducer';
@@ -16,12 +14,7 @@ import {
  createOrganisation,
  updateOrganisation,
  deleteOrganisation,
- // fetchResourcesAgainstOrganizationsForSaga,
- // updateOrganizationForResourceSaga,
 } from '@/app/services/organisationServices';
-import { Organisation } from '@/app/types/organisationTypes';
-import { Resource } from '@/app/types';
-import { ResourceOrganizationPayload } from '@/app/types/organisationTypes';
 
 // loading all orgs
 function* fetchOrganisationsSaga(): Generator<any, void, any> {
@@ -84,91 +77,6 @@ function* deleteOrganisationSaga(action: any): Generator<any, void, any> {
     yield put(setLoading(false));
   }
 }
-
-
-// function* fetchOrganisationResourcesSaga(
-//   action: any
-// ): Generator<any, void, any> {
-//   const { organisations } = action.payload;
-//   try {
-//     yield put(setLoading(true));
-
-
-//     let allOrganisations: Organisation[] = organisations;
-//     if (!organisations || organisations.length === 0) {
-//       const response = yield call(fetchAllOrganizations);
-//       yield put(setOrganisations(response?.result));
-
-
-//       allOrganisations = response?.result;
-//     }
-
-
-//     const organisationResults = yield all(
-//       allOrganisations.map((organisation: any) =>
-//         call(function* () {
-//           const resourcesPostData = {
-//             'ResourceAllocation.Core/GetOrganizationResources': {
-//               OrganizationId: organisation.Id,
-//             },
-//           };
-//           //@ts-ignore
-//           const resourcesResult = yield call(
-//             fetchResourcesAgainstOrganizationsForSaga,
-//             resourcesPostData
-//           );
-
-
-//           return { resourcesResult, organisation };
-//         })
-//       )
-//     );
-
-
-//     let organisationResourceObject: Record<string, Resource[]> = {};
-//     const formatedOrganisationResults = organisationResults
-//       // @ts-ignore
-//       ?.map(organisationResult => ({
-//         id: organisationResult?.organisation?.Id,
-//         resource: organisationResult?.resourcesResult?.result,
-//       }));
-
-
-//     formatedOrganisationResults.forEach(
-//       (payload: { id: string; resource: Resource[] }) => {
-//         organisationResourceObject = organisationResourceObject || {};
-//         organisationResourceObject[payload.id] = payload.resource;
-//       }
-//     );
-
-
-//     if (organisationResults) {
-//       yield put(setAllOrganisationsResources(formatedOrganisationResults));
-//     }
-//   } catch (err) {
-//     console.error('Saga: Failed to fetch organisations resources', err);
-//   } finally {
-//     yield put(setLoading(false));
-//   }
-// }
-
-
-// function* updateOrganizationForResourceSaga(
-//   action: any
-// ): Generator<any, void, any> {
-//   try {
-//     yield put(setLoading(true));
-
-
-//     const response = yield call(updateOrganizationForResourceSaga, action.payload);
-//     // yield put(setOrganisations(response?.result));
-//   } catch (error) {
-//     console.error('Saga error, Failed to update organisation resources: ', error);
-//   } finally {
-//     yield put(setLoading(false));
-//   }
-// }
-
 
 export function* organizationsSaga() {
  yield takeEvery(FETCH_ORGANISATIONS, fetchOrganisationsSaga);
