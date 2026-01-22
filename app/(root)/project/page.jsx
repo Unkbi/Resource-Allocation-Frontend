acba0722a85dd49c31ab703dc740895a6f8d0550
sumitkm7 <sumit.kumar@qtsolv.com>
2026-01-22 12:24:36 +0530

    added timestamp for created on field on project and resource page

--- app/(root)/project/page.jsx
+++ app/(root)/project/page.jsx
@@ -772,7 +772,13 @@ function Project({ permissions, loadingPermissions }) {
           const month = String(date.getMonth() + 1).padStart(2, '0');
           const year = date.getFullYear();
           if (month === 'NaN' || day === 'NaN' || year === 'NaN') return '';
-          return `${month}/${day}/${year}`;
+          const hours = date.getHours();
+          const minutes = String(date.getMinutes()).padStart(2, '0');
+          const AM__PM = hours >= 12 ? 'PM' : 'AM';
+          let hour12 = hours % 12;
+          if (hour12 === 0) hour12 = 12;
+          const hourStr = String(hour12).padStart(2, '0');
+          return `${month}/${day}/${year}  ${hourStr}:${minutes} ${AM__PM}`;
         }
         return '';
       },
